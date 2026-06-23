eb9378b1cdc7446009a3efa029d020b853c428ce
Flovoelkel <33866685+Flovoelkel@users.noreply.github.com>
2026-06-23 14:15:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// ROUTES EINBINDEN
+const portfolioRoutes = require("./routes/portfolio");
+app.use("/api/portfolio", portfolioRoutes);
+
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.json({ status: "wealth-os online" });
 });
